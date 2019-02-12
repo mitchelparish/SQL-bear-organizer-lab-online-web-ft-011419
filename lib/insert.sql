afc1b87247dac095a2ca731ecc06858147474b94
Mitchel Parish <mitchelparish@gmail.com>
2019-02-12 15:33:23 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -3,6 +3,6 @@ INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy'
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 16, 'female');
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 7, 'male');
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 8, 'female');
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 9, 'Scottish Fold');
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 2, 'Scottish Fold');
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 12, 'Scottish Fold');
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 9, 'male');
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 2, 'female');
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 12, 'unknown');
